ome/openmicroscopy

Change constant.value to constant.ordinal in insight

components/insight/SRC/org/openmicroscopy/shoola/env/data/OmeroImageService.java
@@ -78,14 +78,14 @@ public interface OmeroImageService
 	
 	/** Identifies the <code>Maximum intensity</code> projection. */
 	public static final int	MAX_INTENSITY = 
-									ProjectionType.MAXIMUMINTENSITY.value();
+									ProjectionType.MAXIMUMINTENSITY.oridinal();
 	
 	/** Identifies the <code>Mean intensity</code> projection. */
 	public static final int	MEAN_INTENSITY = 
-								ProjectionType.MEANINTENSITY.value();
+								ProjectionType.MEANINTENSITY.oridinal();
 	
 	/** Identifies the <code>Sum intensity</code> projection. */
-	public static final int	SUM_INTENSITY = ProjectionType.SUMINTENSITY.value();
+	public static final int	SUM_INTENSITY = ProjectionType.SUMINTENSITY.oridinal();
 	
 	/** Identifies the type used to store pixel values. */
 	public static final String INT_8 = "int8";

components/insight/SRC/org/openmicroscopy/shoola/env/data/model/ProjectionParam.java
@@ -363,11 +363,11 @@ public class ProjectionParam
 	 */
 	public static ProjectionType convertType(int type)
 	{
-		if (ProjectionType.MAXIMUMINTENSITY.value() == type)
+		if (ProjectionType.MAXIMUMINTENSITY.oridinal() == type)
 			return ProjectionType.MAXIMUMINTENSITY;
-		if (ProjectionType.MEANINTENSITY.value() == type)
+		if (ProjectionType.MEANINTENSITY.oridinal() == type)
 			return ProjectionType.MEANINTENSITY;
-		if (ProjectionType.SUMINTENSITY.value() == type)
+		if (ProjectionType.SUMINTENSITY.oridinal() == type)
 			return ProjectionType.SUMINTENSITY;
 		return null;
 	}

ome/omero-insight

SRC/org/openmicroscopy/shoola/env/data/OmeroImageService.java
@@ -78,14 +78,14 @@ public interface OmeroImageService
 	
 	/** Identifies the <code>Maximum intensity</code> projection. */
 	public static final int	MAX_INTENSITY = 
-									ProjectionType.MAXIMUMINTENSITY.value();
+									ProjectionType.MAXIMUMINTENSITY.ordinal();
 	
 	/** Identifies the <code>Mean intensity</code> projection. */
 	public static final int	MEAN_INTENSITY = 
-								ProjectionType.MEANINTENSITY.value();
+								ProjectionType.MEANINTENSITY.ordinal();
 	
 	/** Identifies the <code>Sum intensity</code> projection. */
-	public static final int	SUM_INTENSITY = ProjectionType.SUMINTENSITY.value();
+	public static final int	SUM_INTENSITY = ProjectionType.SUMINTENSITY.ordinal();
 	
 	/** Identifies the type used to store pixel values. */
 	public static final String INT_8 = "int8";

SRC/org/openmicroscopy/shoola/env/data/model/ProjectionParam.java
@@ -363,11 +363,11 @@ public class ProjectionParam
 	 */
 	public static ProjectionType convertType(int type)
 	{
-		if (ProjectionType.MAXIMUMINTENSITY.value() == type)
+		if (ProjectionType.MAXIMUMINTENSITY.ordinal() == type)
 			return ProjectionType.MAXIMUMINTENSITY;
-		if (ProjectionType.MEANINTENSITY.value() == type)
+		if (ProjectionType.MEANINTENSITY.ordinal() == type)
 			return ProjectionType.MEANINTENSITY;
-		if (ProjectionType.SUMINTENSITY.value() == type)
+		if (ProjectionType.SUMINTENSITY.ordinal() == type)
 			return ProjectionType.SUMINTENSITY;
 		return null;
 	}