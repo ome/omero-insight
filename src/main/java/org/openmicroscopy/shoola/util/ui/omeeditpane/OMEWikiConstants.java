26b309b44a0ed42e312838f79ac47ade890ebaf8
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-09-16 12:00:04 +0100

    handle zero-or-one space in link

--- src/main/java/org/openmicroscopy/shoola/util/ui/omeeditpane/OMEWikiConstants.java
+++ src/main/java/org/openmicroscopy/shoola/util/ui/omeeditpane/OMEWikiConstants.java
@@ -71,22 +71,22 @@ public class OMEWikiConstants
 	/** Regular expression defining Dataset [Dataset: 30]. */
 	//static final String DATASETREGEX = "\\[(Dataset|dataset):[ ]*"+NUMBERREGEX+"[ ]*\\]";
 
-	static final String DATASETREGEX =  "(Dataset|dataset) (ID|id): ("+NUMBERREGEX+")";
+	static final String DATASETREGEX =  "(Dataset|dataset) (ID|id):\\s?("+NUMBERREGEX+")";
 	
 	/** Regular expression defining Project [Project: 30]. */
 	//static final String PROJECTREGEX = "\\[(Project|project):[ ]*"+NUMBERREGEX+"[ ]*\\]";
 	
-	static final String PROJECTREGEX =  "(Project|project) (ID|id): ("+NUMBERREGEX+")";
+	static final String PROJECTREGEX =  "(Project|project) (ID|id):\\s?("+NUMBERREGEX+")";
 	
 	/** Regex expression defining Image [Image: 30]. */
 	//static final String IMAGEREGEX = "\\[(Image|image):[ ]*"+NUMBERREGEX+"[ ]*\\]";
 	
-	static final String IMAGEREGEX =  "(Image|image|Image's) (ID|id): ("+NUMBERREGEX+")";
+	static final String IMAGEREGEX =  "(Image|image|Image's) (ID|id):\\s?("+NUMBERREGEX+")";
 	
 	/** Regex expression defining Protocol [Protocol: id 30]. */
 	//static final String PROTOCOLREGEX = "\\[(Protocol|Protocol):[ ]*"+NUMBERREGEX+"[ ]*\\]";
 
-	static final String PROTOCOLREGEX =  "(Protocol|protocol) (ID|id): ("+NUMBERREGEX+")";
+	static final String PROTOCOLREGEX =  "(Protocol|protocol) (ID|id):\\s?("+NUMBERREGEX+")";
 	
 	/** Regular expression expression defining Wiki Heading. */
 	static final String HEADINGREGEX = "(^[=]{3}[ ]+"+SENTENCEREGEX+"[ ]+[=]{3}[ ]*$|^[=]{2}[ ]+"+SENTENCEREGEX+"[ ]+[=]{2}[ ]*$|^[=]{1}[ ]+"+SENTENCEREGEX+"[ ]+[=]{1}[ ]*$)";
