7820412896074234aa70aac96f5a2efb7516b62a
sukunis <susebo@gmail.com>
2020-04-30 17:10:59 +0200

    Bump version number to 1.1.0

--- src/main/java/org/openmicroscopy/shoola/agents/fsimporter/mde/MDEHelper.java
+++ src/main/java/org/openmicroscopy/shoola/agents/fsimporter/mde/MDEHelper.java
@@ -47,7 +47,7 @@ import org.openmicroscopy.shoola.agents.fsimporter.mde.util.TagData;
 public class MDEHelper {
 
 	public static final String APPLICATION_NAME="MDE";
-	public static final String VERSION="1.0.0";
+	public static final String VERSION="1.1.0";
 
 	public static final String SELECTOR="#";
 
