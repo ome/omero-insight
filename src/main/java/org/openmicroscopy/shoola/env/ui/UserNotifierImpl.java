4ac6cf2eb85a5c972f2426d88fb9357a10a7fc67
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-09-11 09:16:29 +0100

    handle null folder

--- src/main/java/org/openmicroscopy/shoola/env/ui/UserNotifierImpl.java
+++ src/main/java/org/openmicroscopy/shoola/env/ui/UserNotifierImpl.java
@@ -103,6 +103,12 @@ public class UserNotifierImpl implements UserNotifier, PropertyChangeListener {
 	 * @return See above.
 	 */
 	private boolean canWriteInFolder(File folder) {
+		if (folder == null) {
+			notifyInfo("Download file",
+					"The selected folder cannot be null.\nPlease"
+							+ " select a different one.");
+			return false;
+		}
 		if (!folder.canWrite()) {
 			notifyInfo("Download file",
 					"The selected folder is write protected.\nPlease"
