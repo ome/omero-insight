28f80f6fcfbba1431c6587d5bd0adfe18a97c6c7
sukunis <susebo@gmail.com>
2021-01-08 13:40:12 +0100

    add ontology parser to load fixed terms for values from given ontology term ID

---
+++ src/main/java/org/openmicroscopy/shoola/agents/fsimporter/mde/util/parser/BioPortal_Parser.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) <2016-2019> University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openmicroscopy.shoola.agents.fsimporter.mde.util.parser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+/**
+ * 1/8/2021
+ *
+ * @author Susanne Kunis<susannekunis at gmail dot com>
+ *
+ *  output inside a browser see for example: https://data.bioontology.org/ontologies/BRO/
+ *  see also: https://github.com/molgenis/ontocat/blob/master/ontoCAT/src/uk/ac/ebi/ontocat/bioportal/BioportalOntologyService.java
+ *  doc: https://www.bioontology.org/wiki/CTS2_BioPortal_Rest_mapping#Get_a_specific_ontology_based_on_a_version_id
+ *  doc: https://data.bioontology.org/documentation
+ **/
+public class BioPortal_Parser extends OntologyParser {
+    static final String REST_URL = "http://data.bioontology.org";
+    static final String API_KEY = "c6ae1b27-9f86-4e3c-9dcf-087e1156eabe";
+
+    @Override
+    protected String formatURL(String ontology_acronym, String termID_href) {
+        String id_utf8 = null;
+        try{
+            id_utf8= URLEncoder.encode(termID_href, "UTF-8");
+        }catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return REST_URL+"/ontologies/"+ontology_acronym+"/classes/"+id_utf8+"/children";
+    }
+
+    @Override
+    protected ArrayList<String> getLabels(JsonNode ontology_node, String parentLabel) {
+        if(ontology_node==null){
+            return null;
+        }
+        ArrayList<String> labels = new ArrayList<String>();
+        // From the returned page, get the hypermedia link to the next page
+        String nextPage = ontology_node.get("links").get("nextPage").asText();
+
+        // Iterate over the available pages adding labels from all classes
+        // When we hit the last page, the while loop will exit
+        while (nextPage.length() != 0) {
+            for (JsonNode cls : ontology_node.get("collection")) {
+                if (!cls.get("prefLabel").isNull()) {
+                    labels.add(parentLabel+cls.get("prefLabel").asText());
+                }
+                if(!cls.get("links").get("children").isNull()){
+                    labels.addAll(getLabels(getNode(cls.get("links").get("children").asText()),parentLabel+cls.get("prefLabel").asText()+":"));
+                }
+            }
+
+            if (ontology_node.get("totalCount").asLong()>0 && !ontology_node.get("links").get("nextPage").isNull()) {
+                nextPage = ontology_node.get("links").get("nextPage").asText();
+                ontology_node = getNode(nextPage);
+            } else {
+                nextPage = "";
+            }
+        }
+        return labels;
+    }
+
+    @Override
+    protected HttpURLConnection initURLConnection(URL url) throws Exception {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "apikey token=" + API_KEY);
+        conn.setRequestProperty("Accept", "application/json");
+        return conn;
+    }
+}
---
+++ src/main/java/org/openmicroscopy/shoola/agents/fsimporter/mde/util/parser/OntologyParser.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) <2016-2019> University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openmicroscopy.shoola.agents.fsimporter.mde.util.parser;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * 1/8/2021
+ *
+ * @author Susanne Kunis<susannekunis at gmail dot com>
+ **/
+public abstract class OntologyParser {
+    String REST_URL;
+    static final ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     *
+     * @param ontology_acronym acronym of ontology that the term is part of
+     * @param termID_href href
+     * @return labels of all subclasses of given termID
+     */
+    public ArrayList<String> getSubLabels(String ontology_acronym,String termID_href){
+
+        JsonNode ontology_node = getNode(formatURL(ontology_acronym,termID_href));
+        ArrayList<String> labels=getLabels(ontology_node,"");
+//        if(labels!=null) {
+//            // Print out all the labels
+//            for (String label : labels) {
+//                System.out.println(label);
+//            }
+//        }
+        return labels;
+    }
+
+    /**
+     * Get ontology from the REST service and parse the JSON
+     * @param url
+     * @return
+     */
+    JsonNode getNode(String url){
+        String ontology_string=get(url);
+        JsonNode ontology=jsonToNode(ontology_string);
+        //System.out.println("Json onto : "+ontology);
+        if(ontology==null){
+            System.out.println("Can't parse ontology from "+ontology_string);
+            return null;
+        }
+        return ontology;
+    }
+
+    static JsonNode jsonToNode(String json) {
+        JsonNode root = null;
+        try {
+            root = mapper.readTree(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return root;
+    }
+
+    private  String get(String urlToGet) {
+        URL url;
+        HttpURLConnection conn;
+        BufferedReader rd;
+        String line;
+        String result = "";
+        try {
+            url = new URL(urlToGet);
+            conn = initURLConnection(url);
+            //System.out.println("URL: "+conn.getURL());
+            rd = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                result += line;
+            }
+            rd.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    protected abstract String formatURL(String ontology_acronym, String termID_href);
+    protected abstract ArrayList<String> getLabels(JsonNode ontology_node,String parentLabel);
+    protected abstract HttpURLConnection initURLConnection(URL url) throws Exception;
+}
