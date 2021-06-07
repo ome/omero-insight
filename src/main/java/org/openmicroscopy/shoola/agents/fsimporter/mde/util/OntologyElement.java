eeae141087fd2c90b909ce60009f6ae16abbf099
sukunis <sukunis@uos.de>
2021-06-07 13:48:40 +0200

    add copyright and author to OntologyElement class

--- src/main/java/org/openmicroscopy/shoola/agents/fsimporter/mde/util/OntologyElement.java
+++ src/main/java/org/openmicroscopy/shoola/agents/fsimporter/mde/util/OntologyElement.java
@@ -1,7 +1,29 @@
+/*
+ * Copyright (C) <2016-2019> University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openmicroscopy.shoola.agents.fsimporter.mde.util;
 
 import org.openmicroscopy.shoola.agents.fsimporter.ImporterAgent;
 
+/**
+ * 6/5/2021
+ * @author Susanne Kunis<susannekunis at gmail dot com>
+ */
 public class OntologyElement {
     private String val;
     private String id;
